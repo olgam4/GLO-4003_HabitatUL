becfe12a7c1ae581c3f4fd401bb0453e8c794d84
Samuel Cabral Cruz <samuelcabralcruz@gmail.com>
2019-09-26 03:24:56 -0400

    chore (#71): set up dependencies quotes (#72)

--- HabitatUL/src/main/java/ca/ulaval/glo4003/context/ProdContext.java
+++ HabitatUL/src/main/java/ca/ulaval/glo4003/context/ProdContext.java
@@ -1,6 +1,25 @@
 package ca.ulaval.glo4003.context;
 
+import ca.ulaval.glo4003.shared.domain.ClockProvider;
+import ca.ulaval.glo4003.shared.domain.Premium;
+import ca.ulaval.glo4003.shared.infrastructure.SystemUtcClockProvider;
+import ca.ulaval.glo4003.underwriting.domain.PremiumCalculator;
+import ca.ulaval.glo4003.underwriting.domain.quote.QuoteRepository;
+import ca.ulaval.glo4003.underwriting.domain.quote.QuoteValidityPeriodProvider;
+import ca.ulaval.glo4003.underwriting.infrastructure.DummyPremiumCalculator;
+import ca.ulaval.glo4003.underwriting.infrastructure.quote.ConfigBasedQuoteValidityPeriodProvider;
+import ca.ulaval.glo4003.underwriting.persistence.quote.InMemoryQuoteRepository;
+
+import java.math.BigDecimal;
+
 public class ProdContext implements Context {
   @Override
-  public void execute() {}
+  public void execute() {
+    Premium hardCodedPremium = new Premium(new BigDecimal(534.32423423423));
+    ServiceLocator.register(PremiumCalculator.class, new DummyPremiumCalculator(hardCodedPremium));
+    ServiceLocator.register(
+        QuoteValidityPeriodProvider.class, new ConfigBasedQuoteValidityPeriodProvider());
+    ServiceLocator.register(ClockProvider.class, new SystemUtcClockProvider());
+    ServiceLocator.register(QuoteRepository.class, new InMemoryQuoteRepository());
+  }
 }
---
+++ HabitatUL/src/main/java/ca/ulaval/glo4003/shared/infrastructure/SystemUtcClockProvider.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo4003.shared.infrastructure;
+
+import ca.ulaval.glo4003.shared.domain.ClockProvider;
+
+import java.time.Clock;
+
+public class SystemUtcClockProvider implements ClockProvider {
+  @Override
+  public Clock getClock() {
+    return Clock.systemUTC();
+  }
+}
